263e42ed4260732b5d5f4654e6cea51a4189c618
Nicolas Ravain <nicolasravain59@gmail.com>
2020-01-30 10:15:08 +0100

    base projet

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('index/homepage.html.twig');
+      
+    }
+}
